8137d0a6568713a27828e78d5d77f13ed515f726
Remi Launay <rlaunay25@gmail.com>
2021-02-02 14:31:50 +0100

    select class without custom element + sorting

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import photographers from "./data/photographers.json";
 
 import PhotographersList from './js/customElements/PhotographerList'
 import PhotographItem from './js/customElements/PhotographItem'
-import AppSelect from "./js/customElements/AppSelect"
+import Select from "./js/photographePage/Select";
 import AppMedia from "./js/customElements/AppMedia/AppMedia"
 import AppPhoto from "./js/customElements/AppMedia/AppPhoto"
 import AppVideo from "./js/customElements/AppMedia/AppVideo"
@@ -14,10 +14,11 @@ import './scss/main.scss'
 customElements.define('photographers-list', PhotographersList, {extends: 'ul',})
 customElements.define('photograph-item', PhotographItem, { extends: 'li' })
 
-customElements.define('app-select', AppSelect, { extends: 'select' })
-
 customElements.define('app-media', AppMedia)
 customElements.define('app-video', AppVideo)
 customElements.define('app-photo', AppPhoto)
+
+const selectElements = document.querySelectorAll('[data-custom-select]')
+selectElements.forEach(selectElement => new Select(selectElement))
 
 updatePhotographeInfo(photographers)
